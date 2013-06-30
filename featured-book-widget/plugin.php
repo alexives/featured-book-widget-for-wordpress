bd10fd2d47509952da07cf9934c73b8a74477c83
aives <alexives@acm.org>
2013-06-30 12:34:16 -0500

    Misc updates. Fixed problem where isbn was not being sent to google api. Changed font size on Title to be larger. Added sub-titles.

--- featured-book-widget/plugin.php
+++ featured-book-widget/plugin.php
@@ -4,7 +4,7 @@
   Plugin URI: http://alexiv.es
   Description: A widget for featuring books in a widget. Takes an isbn and pulls the cover and other info from google.
   Author: Alex Ives
-  Version: 1
+  Version: 1.1
   Author URI: http://alexiv.es
 */
 
@@ -19,7 +19,7 @@ class FeaturedBookWidget extends WP_Widget {
 				'description' => 'Displays the cover and Author of a featured book.' 
 		);
 		// Uses the existing WP_Widget constructor
-		$this->WP_Widget ( 'FeaturedBookWidget', 'Featured Book and Cover', $widget_ops );
+		$this->WP_Widget ( 'FeaturedBookWidget', 'Featured Book', $widget_ops );
 	}
 	
 	/*
@@ -32,11 +32,15 @@ class FeaturedBookWidget extends WP_Widget {
 				'isbn' => '',
 				'link' => '',
 				'author' => '',
-				'bookTitle' => '' 
+				'bookTitle' => '',
+				'image' => '',
+				'gbLink' => '',
+				'subTitle' => ''
 		) );
 		$title = $instance ['title'];
 		$isbn = $instance ['isbn'];
 		$link = $instance ['link'];
+		$bookTitle = $instance['bookTitle'];
 		// Outputs the form HTML
 		?>
 <p>
@@ -59,19 +63,37 @@ class FeaturedBookWidget extends WP_Widget {
 		name="<?php echo $this->get_field_name('link'); ?>" type="text"
 		value="<?php echo attribute_escape($link); ?>" /></label>
 </p>
+<p>
+	Book Title: <?php echo $bookTitle; ?>
+</p>
 
 <?php
 	}
 	
 	/*
 	 * update is used when the user saves new settings in the
-	 * widget preferences. 
+	 * widget preferences. Also, pulls data from google and adds
+	 * it to the database. This means that it won't have to pull 
+	 * data every time someone loads a page, just an image from 
+	 * the client side. 
 	 */
 	function update($new_instance, $old_instance) {
 		$instance = $old_instance;
 		$instance ['title'] = $new_instance ['title'];
 		$instance ['isbn'] = $new_instance ['isbn'];
 		$instance ['link'] = $new_instance ['link'];
+		// Gets information about the book from the google books api based on isbn.
+		$data = json_decode ( file_get_contents ( 'https://www.googleapis.com/books/v1/volumes?q=isbn:' . $instance ['isbn'], 0, null, null ) );
+		$instance['bookTitle'] = $data->items [0]->volumeInfo->title;
+		$instance['subTitle'] = $data->items [0]->volumeInfo->subtitle;
+		$instance['image'] = $data->items [0]->volumeInfo->imageLinks->thumbnail;
+		$instance['gbLink'] = $data->items [0]->volumeInfo->infoLink;
+		// makes a string list of authors in case there are many.
+		$authors = $data->items [0]->volumeInfo->authors;
+		$instance['author'] = $authors [0];
+		for($i = 1; $i < count ( $authors ); $i ++) {
+			$instance['author'] += " and " . $authors [$i];
+		}
 		return $instance;
 	}
 	
@@ -84,17 +106,11 @@ class FeaturedBookWidget extends WP_Widget {
 		$title = empty ( $instance ['title'] ) ? ' ' : apply_filters ( 'widget_title', $instance ['title'] );
 		$isbn = $instance ['isbn'];
 		$link = $instance ['link'];
-		// Gets information about the book from the google books api based on isbn.
-		$data = json_decode ( file_get_contents ( 'https://www.googleapis.com/books/v1/volumes?q=isbn:' . $isbn, 0, null, null ) );
-		$bookTitle = $data->items [0]->volumeInfo->title;
-		$thumbnail = $data->items [0]->volumeInfo->imageLinks->thumbnail;
-		$gbLink = $data->items [0]->volumeInfo->infoLink;
-		// makes a string list of authors in case there are many.
-		$authors = $data->items [0]->volumeInfo->authors;
-		$author = $authors [0];
-		for($i = 1; $i < count ( $authors ); $i ++) {
-			$author += " and " . $authors [$i];
-		}
+		$gbLink = $instance['gbLink'];
+		$thumbnail = $instance['image'];
+		$bookTitle = $instance['bookTitle'];
+		$author = $instance['author'];
+		$subTitle = empty( $instance['subTitle']) ? '' : ": ".$instance['subTitle'];
 		// if the link is empty, use the link to the google books page.
 		if (empty ( $link )) {
 			$link = $gbLink;
@@ -104,8 +120,8 @@ class FeaturedBookWidget extends WP_Widget {
 		// if the title is empty, don't display it.
 		if (! empty ( $title ))
 			echo $before_title . $title . $after_title;
-		echo ("<a href='$link'><span class='fbw-thumbnail'><img src='$thumbnail' /></span><br /><span class='fbw-title'>$bookTitle</span><br /><span class='fbw-author'>by $author</span></a>");
-		echo ("<br /><a href='$gbLink' target='_BLANK'>Powered by Google</a>");
+		echo ("<a href='$link'><span class='fbw-thumbnail'><img src='$thumbnail' /></span><br /><span style='font-size: 1.3em' class='fbw-title'>$bookTitle</span><span style='font-size: 1.1em' class='fbw-subTitle'>$subTitle</span><br /><span class='fbw-author'>by $author</span></a>");
+		echo ("<br /><a style='font-size: .65em' href='$gbLink' target='_BLANK'>Powered by Google</a>");
 		echo $after_widget;
 	}
 }
